6b37f6d5dfaceae6e53bef5b8f5f422f9a16b5e3
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-09 10:05:45 +0500

    Initial commit

---
+++ ap.js
@@ -0,0 +1,152 @@
+//============= project-01 show alert of favourite animal==========
+ var animal = "Goat";
+  alert("My favourite animal is" + " " + animal);
+// =================== project-02 Math challenge===========
+  var a = 10;
+  var b = 4;
+  var c = 2;
+  var result = (a + b) * c;
+  alert("The final result is:" + " " + result);
+
+// ===========project-03 change cityName=============
+  var city = "karachi";
+  alert("I live in " + city);
+ city = "Mianwali";
+  alert("Now i live in " + city);
+
+// ==================project-03, Simple Bio Card===============
+  var name = "Azizullah";
+  var age = 25;
+ var hobby = "Fish hunting";
+  alert("Name: " + name + "\nAge: " + age + "\nhobby: " + hobby);
+
+// ========project-04...Welcome Message
+
+ var myName = "Azizullah";
+ alert("Welcome " + myName + " to our website!");
+
+// ===========project-05- Simple Math Calculation
+
+ var num1 = 10;
+ var num = 20;
+ var total = num1 + num;
+ alert("The total is: " + total)
+
+// project-06 Overwriting variables
+
+ var myCurrentFavouriteColor = "pink";
+ alert("My favourite color is " + myCurrentFavouriteColor);
+ myCurrentFavouriteColor = "blue";
+ alert("Now my favourite color is " + myCurrentFavouriteColor);
+
+// =======project-06- Shopping Total
+
+ var shirtPrice = 200;
+ var shoesPrice = 500;
+ var total_price = shirtPrice + shoesPrice ;
+ alert("The total price is: Rs. " + total_price);
+
+// ===============project-06- Animal love
+
+ var animal = "goat";
+ alert("I love " + animal);
+
+// project-07 Math Trick;
+
+ var number1 = 5;
+ var number2 = 3;
+ var number3 = 10;
+ var Calculation = (number1 + number2) * number3;
+ console.log(Calculation);
+ alert("The final result is: " + Calculation);
+
+// project-07 city change
+
+ var cityName = "karachi";
+ alert("I live in " + cityName);
+ cityName = "Lahore";
+ alert("Now i live in " + cityName);
+
+// project-08 Personal Info &  Shopping Summary
+
+ var myName = "Azizullah";
+ var age = 33;
+ var favouriteColor = "pink";
+ var bagPrice = 500;
+ var watchPrice = 1000;
+ var totalCost = bagPrice + watchPrice;
+ console.log(totalCost);
+ alert("Name: " + myName + "\nAge: " + age + "\nFavourite Color: " + favouriteColor);
+
+ alert("Item 1: Rs. " + bagPrice + "," + " Item 2: Rs. " + watchPrice);
+
+ alert("Total Price: Rs. " + totalCost);
+
+//==============project-09 Movie ticket price
+
+ var movieName = "Pakistan Justice-System";
+ var ticketPrice = 2000;
+ alert("The ticket price of " + movieName + " is Rs. " + ticketPrice);
+
+// ============project-10 Scoreboard==============
+ var playerName = "Mubin";
+ var score = 100;
+alert(playerName + " scored " + score + " points in the match!");
+
+//========== Age in Month==============
+ 
+ var myAge = 30;
+ var ageInMonth = myAge * 12;
+ console.log(ageInMonth);
+ alert("You are " + ageInMonth + " months old");
+
+// ===========project-11 Full Name Builder Concatenation 
+
+ var myFirstName = "Muhammad";
+ var myLastName  = "Nadeem";
+ var fullName = myFirstName + myLastName;
+ alert("Your full Name is:  " + myFirstName + " " + myLastName);
+
+// ==============project-12 String Concatenation Project  Address Card
+
+ var houseNumber = 222;
+ var streetNumber = 4;
+ var city = "Mianwali";
+ alert("House #" + houseNumber + ", " + "Street " + streetNumber + ", " + city);
+
+// ==========project-13 Conactenation travel plan====================
+
+ var destination = "Mianwali";
+ var date = "May 05-2025";
+ var transport = "Bus";
+ alert("You are going to " + destination + " by " + transport + " on " + date);
+
+// ============project-14 concatenation of strings
+
+ var myFirstName = "Muhammad";
+ var myLastName = "Saqib";
+ var fullName = myFirstName + " " + myLastName;
+console.log(fullName);
+ alert(fullName);
+
+// =========project-15 concatenation
+  var subject = "He";
+var verb = "eats";
+  var object = "apples";
+  var sentence = subject + " " +  verb + " " + object;
+ alert(sentence);
+
+// ===============project-16 concatenation 
+
+ var subject = "You";
+ var verb = "want";
+ var another = "to become a";
+ var object = "Software Engineer";
+ var sentence = subject + " " + verb + " " + another + " " + object + ".";
+ alert(sentence);
+
+//  project-17 add two different data types
+  var num1 = 20;
+  var num2 = "5";
+  var newNum = num1 + num2;
+  alert(newNum);
