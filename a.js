2032295b953088f25a134e2298225cb687975f48
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-09 20:55:10 +0500

    update

---
+++ a.js
@@ -0,0 +1,44 @@
+// ===========project 18- My info card=============
+// var myName = "Aziz";
+// var myAge = 30;
+// var myFavouriteNumber = 222;
+// var multiply = myAge * myFavouriteNumber;
+// alert("My name is " + myName + " and i am " + myAge + " years old. " + "My Favourite number is " + myFavouriteNumber);
+
+// ===========project 19- Simple Math quiz===========
+// var num1 = 4;
+// var num2 = 3;
+// var num3 = 2;
+// var total = num1 + num2 + num3;
+// alert(total);
+// ============project 20- Eliminating ambiguity=============;
+//  var Calculation = (3 + 4) - (2*1);
+//  alert(Calculation);
+//  =============project 21 variable overwriting===========
+// var myFavouriteColor = "blue";
+// myFavouriteColor = "Green";
+// alert(myFavouriteColor);
+// ===========project 21 - Creating  two or more variable of same name=========
+// var fruit = "apple"
+// var fruit = "mango";
+// alert(fruit);
+// var fruit = "banana";
+// var fruit = "guava";
+// alert(fruit);
+// ************note do not create two more variables having same name*****************
+// ==============project -22 ---checking type of data===================
+// var myname = "Aziz";
+// var checkDataType = typeof myname;
+// console.log(checkDataType);
+// var number = 98789;
+// var checkDataType = typeof  number;
+// console.log(checkDataType);
+// we can check direct data type also
+//  console.log(typeof "apple");
+//  when we add one string and one number
+// var num1 = 20;
+// var num2 = 2;
+// var sum = num1 + num2;
+// console.log(sum);
+// ============project 23 - Concatenation of strings=========
+ 
